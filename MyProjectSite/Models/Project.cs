c285ae5f2c47d81dd83dc5eb2256082841774177
Robert Murray <alexmurray93@hotmail.com>
2017-11-03 12:40:45 -0700

    Create Project model.

---
+++ MyProjectSite/Models/Project.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyProjectSite.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Language { get; set; }
+
+        public static List<Project> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("/users/ReconScout77/repos?per_page=999", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () => 
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var projectList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse["repos"].ToString());
+            return projectList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
